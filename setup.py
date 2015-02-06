2a5d4941e1bbca8ed9c095ebeab2da2c512d70d3
Colin Chan <colinchan@lumeh.org>
2015-02-06 11:28:35 -0500

    Refactor everything

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup
 
 setup(
     name="rockuefort",
+    version="0.1",
     install_requires=[
-        "docopt ==0.6.1",
-        "mutagenx ==1.22",
+        "docopt >=0.6.1",
+        "mutagen >=1.27",
     ],
     scripts=["rockuefort"],
 )
